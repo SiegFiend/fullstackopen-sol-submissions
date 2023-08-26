d200bf7190fa7c911771d807103f3b70e8eaefef
SiegFiend <phoenx.rave@gmail.com>
2023-08-26 13:00:38 +0100

    first commit

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,28 +1,59 @@
 const App = () => {
+
+  // Course name
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  // Array of parts, each with a name and exercises count
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
+
+  //component to render course name 
+  const Header = ({ course }) => {
+    return (
+      <h1>{course}</h1>
+    );
+  }
+
+   //component to display the part name and exercises count
+  const Part = ({ part, exercises }) => {
+    return (
+      <p>
+        {part} {exercises}
+      </p>
+    );
+  }
+  
+
+  //Map through each part and render using Part component
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map((part, index) => (
+          <Part key={index} part={part.name} exercises={part.exercises} />
+        ))}
+      </div>
+    );
+  }
+
+  //Calculate the total number of exercises using reduce
+  const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+  
+    return (
+      <p>Number of exercises {totalExercises}</p>
+    );
+  }
 
   return (
     <div>
       <Header course={course} />
-      
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
+  );
 }
 
 export default App
